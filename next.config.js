66a5d9c5ac707bff019449f877dc97b9ad2cfb57
남형석 <azerc@MacMini.local>
2022-06-08 15:29:52 +0900

    add Heater FrontEnd

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "s2.coinmarketcap.com",
+      "s3.coinmarketcap.com",
+      "encrypted-tb0.gstatic.com",
+    ],
+  },
+};
