c592bd0ddaeb00ab3ccda35ecc3f094f550e78bb
남형석 <azerc@MacMini.local>
2022-06-09 07:11:08 +0900

    ...

--- README.md
+++ README.md
@@ -21,6 +21,3 @@ pnpm create next-app --example with-tailwindcss with-tailwindcss-app
 ```
 
 Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) ([Documentation](https://nextjs.org/docs/deployment)).
-
-Injected build settings env var: NEXT_PUBLIC_SERVER
-Injected build settings env var: NEXT_PUBLIC_APP_ID
--- components/trendingCardRow.js
+++ components/trendingCardRow.js
@@ -2,10 +2,26 @@ import React from "react";
 import Image from "next/image";
 import Rate from "./cmc-table/rate";
 
-const styles = {};
+const styles = {
+  trendingCardRow: `flex items-center justify-between mb-4 text-[0.93rem]`,
+};
 
-function TrendingCardRow(props) {
-  return <div>TrendingCardRow</div>;
+function TrendingCardRow({ number, symbol, name, icon, isIncrement, rate }) {
+  return (
+    <div className={styles.trendingCardRow}>
+      <p className="opacity-40">{number}</p>
+      <div className="w-full flex">
+        <div className="mx-5">
+          {icon && <Image src={icon} width={20} height={20} alt="icon" />}
+        </div>
+        <p className="font-bold">
+          {name}
+          <span className="text-gray-400">({symbol})</span>
+        </p>
+      </div>
+      <Rate isIncrement={isIncrement} rate={rate} />
+    </div>
+  );
 }
 
 export default TrendingCardRow;
